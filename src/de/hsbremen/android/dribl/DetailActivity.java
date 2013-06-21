b9e776ceb2cd47f6538dfa2d49279e5a29a0e107
Jannes <jannes.meyer@gmail.com>
2013-06-21 18:06:07 +0200

    Enable up button

--- src/de/hsbremen/android/dribl/DetailActivity.java
+++ src/de/hsbremen/android/dribl/DetailActivity.java
@@ -2,6 +2,8 @@ package de.hsbremen.android.dribl;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v4.app.NavUtils;
+import android.view.MenuItem;
 
 public class DetailActivity extends Activity {
 
@@ -11,6 +13,17 @@ public class DetailActivity extends Activity {
 		setContentView(R.layout.activity_detail);
 		
 		getActionBar().setDisplayHomeAsUpEnabled(true);
+	}
+	
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+			// This call is only needed for compatibility with old API levels
+			case android.R.id.home:
+				NavUtils.navigateUpFromSameTask(this);
+				return true;
+		}
+		return super.onOptionsItemSelected(item);
 	}
 
 }
